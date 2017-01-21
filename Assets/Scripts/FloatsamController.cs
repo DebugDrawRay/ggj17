0d69a02a64ef30a7d3e8a2aa4030f5f8bde111a9
seraphki <sephki@gmail.com>
2017-01-21 03:35:37 -0800

    Collecting little un's

---
+++ Assets/Scripts/FloatsamController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatsamController : MonoBehaviour
+{
+	public float collectThreshold;
+
+	protected Collider theCollider;
+	protected Rigidbody theRigidBody;
+
+	protected WaveStatusController attachedWave;
+	protected GameObject attachPoint;
+	protected float speed = 10;
+
+	void Awake()
+	{
+		theCollider = gameObject.GetComponent<Collider>();
+		theRigidBody = gameObject.GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (attachPoint != null)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, attachPoint.transform.position, speed * Time.deltaTime);
+			transform.rotation = attachPoint.transform.rotation;
+			
+			if (attachedWave.scale < collectThreshold)
+			{
+				Debug.Log("DETATCH!");
+				Destroy(attachPoint);
+				attachPoint = null;
+				theRigidBody.isKinematic = false;
+				theRigidBody.useGravity = true;
+			}
+		}
+
+		if (transform.position.y < -10)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void AttachToWave(WaveStatusController waveController, GameObject waveAttachPoint)
+	{
+		attachedWave = waveController;
+		attachPoint = waveAttachPoint;
+		Debug.Log("Wave Attach:" + waveAttachPoint.name);
+		theCollider.enabled = false;
+	}
+}
