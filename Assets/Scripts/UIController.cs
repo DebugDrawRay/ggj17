9d6c92e0ab401be76cf57844d7e5b0d255e26e2e
seraphki <sephki@gmail.com>
2017-01-21 19:24:28 -0800

    Wee little UI

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+	public Text curentHeight;
+
+	void Update()
+	{
+		if (WaveStatusController.instance != null)
+		{
+			curentHeight.text = "HEIGHT: " + Mathf.CeilToInt(WaveStatusController.instance.scale);
+		}
+	}
+}
